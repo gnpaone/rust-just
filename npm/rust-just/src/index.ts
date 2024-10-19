205b7b2f8ed0e6714ce11a8b2fab7533ec3ee2aa
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-10-19 19:55:30 +0530

    Add --execaoptions arg to pass Execa options

--- npm/rust-just/src/index.ts
+++ npm/rust-just/src/index.ts
@@ -3,14 +3,46 @@
 import { execa } from "execa";
 import { fileURLToPath } from "node:url";
 import { getExePath } from "./getExePath.js";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+
+function safeParse(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (e) {
+    console.error("Invalid JSON format in --options");
+    process.exit(1);
+  }
+}
 
 async function run() {
   const exePath = await getExePath();
+
+  const argv = yargs(hideBin(process.argv))
+    .option('execaoptions', {
+      type: 'string',
+      description: 'Execa options in JSON format',
+    })
+    .parserConfiguration({
+      'unknown-options-as-args': true,
+    })
+    .help(false)
+    .version(false)
+    .parse();
+
+  const args = argv._;
+
+  let execaOptions = {
+    stdio: "inherit",
+    reject: false,
+  };
+
+  if (argv.execaoptions) {
+    const userOptions = safeParse(argv.execaoptions);
+    execaOptions = { ...execaOptions, ...userOptions };
+  }
   
-  const args = process.argv.slice(2);
-  const processResult = await execa(fileURLToPath(exePath), args, {
-    stdio: "inherit"
-  });
+  const processResult = await execa(fileURLToPath(exePath), args, execaOptions);
 
   process.exit(processResult.exitCode ?? 0);
 }
